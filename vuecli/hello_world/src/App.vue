7ed2a5163bb2491e532aea29e40385e7332b18cc
liulang223 <t125449705@qq.com>
2019-02-12 21:41:51 +0800

    vue

---
+++ vuecli/hello_world/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>|
+      <router-link to="/mine">我的</router-link>|
+      <router-link to="/test">测试</router-link>|
+       <router-link :to="{name:'test1',params:{name:'Hello',age:'20'}}">测试子路由1</router-link>|
+        <router-link to="/test2/ls/30">测试子路由2</router-link>|
+        <router-link to="/home">返回首页</router-link> |
+        <router-link to="/redirect/ww/40">返回测试子路由2</router-link>|
+        <router-link to="/abc">alias</router-link>|
+    </div>
+    <router-view/>
+  </div>
+</template>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vuecli/hello_world/src/views/Test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>测试子路由页面</h1>
+        <router-view/>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
+
